6cb2eb4698b6982f81d06cc60561392d7b5ff489
Kristofferson Culmer <krcd58@mail.missouri.edu>
2026-06-12 09:20:29 -0500

    Add files via upload

---
+++ challnge_code/vending_machine.py
@@ -0,0 +1,31 @@
+def main():
+    #initialize amount due to 50
+    amount_due = 50
+    print("Vending Machine\n------------------\n")
+
+    #loop until amount due is 0 or less
+    while True:
+        #print the amount due
+        print(f"Amount Due: {amount_due}")
+
+        #ask user to enter a coin
+        coin = input("\nEnter coin: ")
+
+        #determine if coin denomination is valid. if not, reprompt user and continue to the top of the loop
+        if coin!= "1" and coin != "10" and coin != "5" and coin != "25":
+            continue
+
+        #subtract from amount due
+        #amount_due = amount_due - coin
+        amount_due -= int(coin)
+
+        #determine if amount due is 0 or less. If amount due > 0, reprompt the user, else break out of the loop
+        if amount_due <= 0:
+            break
+        else:
+            continue
+    
+    print(f"Change due: {amount_due * -1}")
+
+
+main()
