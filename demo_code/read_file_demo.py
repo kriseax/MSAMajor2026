94e9ce2f84775ffea89d405d31ff2a672c0f1304
Kristofferson Culmer <krcd58@mail.missouri.edu>
2026-06-16 11:32:33 -0500

    Uploaded read_file_demo, menu.txt

---
+++ demo_code/read_file_demo.py
@@ -0,0 +1,17 @@
+def main():
+    #open menu.txt: create a file handler to open file in read mode
+    data_file = open("menu.txt", "r")
+    print(data_file)
+
+    #create an empty dictionary
+    menu_items = {}
+    #use a loop to read the contents of the file line by line
+    for line_of_data in data_file:
+        #split the line at the comma
+        item_name_and_price = line_of_data.split(",")
+        print(item_name_and_price)
+        #get the item and price from the list
+        #create an entry in the dictionary for the item and price
+    #close the file
+
+main()
