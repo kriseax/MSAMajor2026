b1c760f19a0719b798d2077339f2cee28af3790c
Kristofferson Culmer <krcd58@mail.missouri.edu>
2026-06-25 11:42:14 -0500

    Add files via upload

--- challnge_code/student_data_api/app.py
+++ challnge_code/student_data_api/app.py
@@ -65,5 +65,26 @@ def api_get_student_by_id(id:str):
     student = search_dictionary_list("id", id)
     return jsonify(student)
 
+#create a route to return a list of unique majors
+@app.route('/api/majors/all', methods=['GET'])
+def get_all_majors():
+    #create a list to store the majors
+    major_list = []
+
+    #get a list of student dictionaries
+    student_dictionaries = sg.get_student_dictionaries()
+
+    #use a for loop to iterate through the student list
+    for student in student_dictionaries:
+    # add the major to the major list if the major is not already in the list
+        if student['major'] not in major_list:
+            major_list.append(student['major'])
+
+    #sort the list
+    major_list.sort()
+
+    #return the list
+    return major_list
+
 #run the application
 app.run(debug=True)
