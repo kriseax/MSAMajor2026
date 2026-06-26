cab6c215ddc2bf27275208b26bfc3c50d06eccf4
Kristofferson Culmer <krcd58@mail.missouri.edu>
2026-06-26 10:20:37 -0500

    Add files via upload

--- challnge_code/student_data_api/student_generator_v2.py
+++ challnge_code/student_data_api/student_generator_v2.py
@@ -1,5 +1,20 @@
 
 from Student import Student
+from datetime import datetime
+
+"""
+Function to write an error massage to a log file
+Input: (str) error message
+Output: None
+"""
+def write_to_error_log(message:str) -> None:
+    the_date = datetime.now()
+    #open the log file in append mode: error_log.txt
+    with open("error_log.txt", "a") as log_file:
+        log_file.write(f"{the_date}: {message}\n")
+        #write an error message to the file in the format
+        #6/26/2026: Error in data file on line 5
+    return
 
 """
 Function to return a list of student objects
@@ -29,6 +44,7 @@ def load_students() -> list[Student]:
         #if error in format then write to a log file
         try:
             if len(student_data) != 6:
+                write_to_error_log(f"Error on line {line_number} of the file. Data has {len(student_data)} items but should have 6.")
                 raise Exception(f"Error on line {line_number} of the file. Data has {len(student_data)} items but should have 6.\n")
         except Exception as error:
             continue
@@ -42,6 +58,7 @@ def load_students() -> list[Student]:
             gpa = float(student_data[4])
         except:
             print(f"Error on line {line_number} of the file")
+            write_to_error_log(f"Error on line {line_number} of the file: credit_hours or gpa have incorrect format.")
             continue
         student_id = student_data[5].strip()
 
